ed5524f2a4660c037b74f472c39f10ea42907df3
liuyinzhe <54565139+liuyinzhe@users.noreply.github.com>
2021-11-25 16:20:15 +0800

    Update 临时记录.py

--- 临时记录.py
+++ 临时记录.py
@@ -6,6 +6,10 @@ me_min_df=me_min_df.mask(me_min_df.applymap(str).eq('zzz'))
 v_df.loc[v_df['v_type']=='D','v_len']=v_df.loc[v_df['v_type']=='D','v_len'] * -1 
 v_df
 
+v_df = v_df.loc[v_df['v_type']!="S"]
+#修改
+v_df['v_len'] = v_df['v_len'].apply(np.log10)
+
 # 计算添加一列，添加方法是后面按照列(axis=1)连接，后续补充任意位置插入的方法
 new_stat_df=pd.concat([new_stat_df,pd.Series(new_stat_df['v_len']/new_stat_df['r_len'],name="prec")],axis=1)
 
