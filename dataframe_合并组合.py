96525f99fac0e49612c84695b5a01f37ddf5f197
liuyinzhe <54565139+liuyinzhe@users.noreply.github.com>
2021-12-06 15:14:46 +0800

    Update dataframe_合并组合.py

--- dataframe_合并组合.py
+++ dataframe_合并组合.py
@@ -14,3 +14,9 @@ merge_df = merge_df.append(b_stat_df.iloc[0:3,:])
 # pd.concat() 默认 axis=0，行叠加，axis=1 ，为列叠加
 # append的参数即可以是一个DataFrame，也可以是一个数组，这种情况下可以合并多个DataFrame
 merge_df = pd.concat([df, df2])
+
+
+
+#### 重要，合并后要将index 重置下，从新从0 数，否则一些智障封包会报错
+# ValueError: cannot reindex from a duplicate axis
+df.reset_index()
