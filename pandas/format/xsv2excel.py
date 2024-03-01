7b29a8f2adbee7d0412fd71c87da6b69249f3486
liuyinzhe <54565139+liuyinzhe@users.noreply.github.com>
2024-03-01 09:08:13 +0800

    Update xsv2excel.py

--- pandas/format/xsv2excel.py
+++ pandas/format/xsv2excel.py
@@ -1,11 +1,12 @@
+import sys
 import pandas as pd
 from pathlib import Path
 def xsv2xlsx(xsv_path):
-    suffix_lst = ['.tsv','.csv']
+    suffix_lst = ['.tsv','.xls','.csv']
     input_path=Path(xsv_path)
     file_suffix = input_path.suffix
     assert file_suffix in suffix_lst,"This suffix is not an allowed input suffix"
-    if file_suffix == '.tsv':
+    if file_suffix == '.tsv' or file_suffix == '.xls':
         dataframe = pd.read_csv(input_path,sep='\t')
     elif file_suffix == '.csv':
         dataframe = pd.read_csv(input_path,sep=',')
@@ -19,7 +20,7 @@ def main():
     # script_dir = Path(script_path).parent
     # print(script_dir)
     current_dir = Path.cwd()
-    example_path = Path.joinpath(current_dir,'example.csv')
+    example_path = Path.joinpath(current_dir,sys.argv[1])
     xsv2xlsx(example_path)
     
 if __name__ == "__main__":
