3f87e5ce06e837ad303df165751b67a686e62c79
liuyinzhe <54565139+liuyinzhe@users.noreply.github.com>
2025-02-06 10:34:51 +0800

    Update xsv2excel.py

--- pandas/format/xsv2excel.py
+++ pandas/format/xsv2excel.py
@@ -1,6 +1,26 @@
-import sys
+#import sys
 import pandas as pd
 from pathlib import Path
+
+
+def GetAllFilePaths(pwd,wildcard='*'):
+    '''
+    获取目录下文件全路径，通配符检索特定文件名，返回列表
+    param: str  "pwd"
+    return:dirname pathlab_obj
+    return:list [ str ]
+    #https://zhuanlan.zhihu.com/p/36711862
+    #https://www.cnblogs.com/sigai/p/8074329.html
+    '''
+    files_lst = []
+    target_path=Path(pwd)
+    for child in target_path.rglob(wildcard):
+        if child.is_dir():
+            pass
+        elif child.is_file():
+            files_lst.append(child)
+    return files_lst
+
 def xsv2xlsx(xsv_path):
     suffix_lst = ['.tsv','.xls','.csv']
     input_path=Path(xsv_path)
@@ -20,8 +40,20 @@ def main():
     # script_dir = Path(script_path).parent
     # print(script_dir)
     current_dir = Path.cwd()
-    example_path = Path.joinpath(current_dir,sys.argv[1])
-    xsv2xlsx(example_path)
+    #example_path = Path.joinpath(current_dir,sys.argv[1])
+
+    xsv_lst=GetAllFilePaths(current_dir,wildcard='*sv')
+    xls_lst=GetAllFilePaths(current_dir,wildcard='*.xls')
+    for xsv_path in xsv_lst:
+        xsv2xlsx(xsv_path)
+    for xls_path in xls_lst:
+        xsv2xlsx(xls_path)
     
 if __name__ == "__main__":
     main()
+    '''
+    使用：
+       将需要生成excel 文件的 文件对应改为一下后缀  *.tsv *.xls *.csv(,逗号分隔符)，文件需要有表头
+       python3 xsv2excel.py
+       会扫描当前目录下，以及当前目录子目录下的所有有这三个后缀的文件，在当前目录生成对应名字的*.xlsx文件
+    '''
